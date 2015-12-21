6e0a5f4e7b39913a9ffb24d360981a3871279d89
leavesleeve <cyes8439@yahoo.com.tw>
2015-12-21 21:31:25 +0800

    second

--- src/A6.java
+++ src/A6.java
@@ -1,9 +1,48 @@
-
+/*
+ * user name D0280212
+ */
+import java.util.ArrayList;
 public class A6 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//test
+		Component quit = new TestPaper();
+		if (quit instanceof TestPaper) {
+			quit.addQuestion(new Question("88 + 123 = ?"));
+		}
+		quit.doQuestion();
 	}
+}
+abstract class Component {
+	abstract void doQuestion();
+	abstract void addQuestion(Question question);
+}
 
+class Question extends Component {
+	String description;
+	public Question(String desc) {
+		this.description = desc;
+	}
+	public void print() {
+		System.out.println(this.description);
+	}
+	@Override
+	void doQuestion() {
+		print();
+	}
+	@Override
+	void addQuestion(Question question) {
+	}
+}
+
+class TestPaper extends Component {
+	ArrayList<Component> list;
+	public TestPaper() {
+		list = new ArrayList<>();
+	}
+	public void addQuestion(Question q) {
+		list.add(q);
+	}
+	@Override
+	void doQuestion() {
+		list.forEach(Component::doQuestion);
+	}
 }
