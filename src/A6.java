13108f0c9cf0c949a73628584181a0ab2996a2f2
leavesleeve <cyes8439@yahoo.com.tw>
2015-12-21 21:42:47 +0800

    third

--- src/A6.java
+++ src/A6.java
@@ -1,16 +1,9 @@
 /*
  * user name D0280212
+ * 林倚婕
  */
 import java.util.ArrayList;
-public class A6 {
-	public static void main(String[] args) {
-		Component quit = new TestPaper();
-		if (quit instanceof TestPaper) {
-			quit.addQuestion(new Question("88 + 123 = ?"));
-		}
-		quit.doQuestion();
-	}
-}
+
 abstract class Component {
 	abstract void doQuestion();
 	abstract void addQuestion(Question question);
@@ -23,6 +16,7 @@ class Question extends Component {
 	}
 	public void print() {
 		System.out.println(this.description);
+		
 	}
 	@Override
 	void doQuestion() {
@@ -44,5 +38,18 @@ class TestPaper extends Component {
 	@Override
 	void doQuestion() {
 		list.forEach(Component::doQuestion);
+	}
+}
+public class A6 {
+	public static void main(String[] args) {
+		Component quit = new TestPaper();
+		if (quit instanceof TestPaper) {
+			quit.addQuestion(new Question("88 + 123 = ?"));
+			quit.addQuestion(new Question("0 + 1 = ?"));
+		}
+		System.out.println("student: leavesleeve,d0280212	GRADE:");
+		System.out.println("1.100%(50%*2):");
+		quit.doQuestion();
+		
 	}
 }
